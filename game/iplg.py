13dff666a0b191d4e86761afc895491b6ae3d1b5
Alok Pandey <62480319+alokp2@users.noreply.github.com>
2023-03-13 16:17:49 +0530

    Update iplg.py

--- game/iplg.py
+++ game/iplg.py
@@ -1,28 +1,31 @@
 import random
+
 print("Welcome to IPL")
-li = ["CSK","KIXP","KKR","RR","RCB","GT","MI"]
+li = ["CSK", "KIXP", "KKR", "RR", "RCB", "GT", "MI"]
 print(li)
-user=input("Your Name: ")
-oppTeam=random.choice(li)
-print("Opposite Team:",oppTeam)
+user = input("Your Name: ")
+oppTeam = random.choice(li)
+print("Opposite Team:", oppTeam)
 
 '''Toss Time'''
 
-user1= input("Your CHOICE: (H/T):")
-System_toss=random.choice(["H", "T"])
-print("System_toss:",System_toss)
-if(user1==System_toss):
+user1 = input("Your CHOICE: (H/T):")
+System_toss = random.choice(["H", "T"])
+print("System_toss:", System_toss)
+
+if user1 == System_toss:
     print("You Won the Toss")
+    choice = int(input("Select Your Choice(Bat/Ball):"))
+    if choice == 1:
+        print("Bat")
+    elif choice == 2:
+        print("Ball")
+    else:
+        print("This is an invalid number")
 else:
-    print("You lost the Toss")
+    print(random.choice(["Bat", "Ball"]))
 
-
-choice= int(input("Select Your Choice(Bat/Ball):"))
-if(choice==1):
-    print("Bat")
-elif(choice==2):
-    print("Ball")
-else:
-    print("This is an invalid number")
-
-balls=[1,2,3,4,5,6,"WICKET","NoBALL","Wide"]
+total_balls = [0, 1, 2, 3, 4, 5, 6, 'WICKET', 'NoBALL', 'Wide']
+print(type(total_balls))
+for items in total_balls:
+    print(items)
