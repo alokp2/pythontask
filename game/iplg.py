1d6972eb6d59848609f8f768227a4b5df958abf2
Alok Pandey <62480319+alokp2@users.noreply.github.com>
2023-03-13 11:30:25 +0530

    Add files via upload

---
+++ game/iplg.py
@@ -0,0 +1,28 @@
+import random
+print("Welcome to IPL")
+li = ["CSK","KIXP","KKR","RR","RCB","GT","MI"]
+print(li)
+user=input("Your Name: ")
+oppTeam=random.choice(li)
+print("Opposite Team:",oppTeam)
+
+'''Toss Time'''
+
+user1= input("Your CHOICE: (H/T):")
+System_toss=random.choice(["H", "T"])
+print("System_toss:",System_toss)
+if(user1==System_toss):
+    print("You Won the Toss")
+else:
+    print("You lost the Toss")
+
+
+choice= int(input("Select Your Choice(Bat/Ball):"))
+if(choice==1):
+    print("Bat")
+elif(choice==2):
+    print("Ball")
+else:
+    print("This is an invalid number")
+
+balls=[1,2,3,4,5,6,"WICKET","NoBALL","Wide"]
